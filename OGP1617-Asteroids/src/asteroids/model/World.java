619fcb840417bdc48bbf9ce48bdc106c8da03754
Sieben Bocklandt <siebe@192.168.0.205>
2017-03-20 14:29:21 +0100

    Added World Constructor

--- OGP1617-Asteroids/src/asteroids/model/World.java
+++ OGP1617-Asteroids/src/asteroids/model/World.java
@@ -7,4 +7,38 @@ import be.kuleuven.cs.som.annotate.*;
  */
 public class World {
 
+	///CONSTRUCTOR///
+	public World(double width, double height){
+		setWorldWidth(width);
+		setWorldHeight(height);
+		
+	}
+	
+	public World() {
+		this(UPPER_WORLD_BOUND,UPPER_WORLD_BOUND);
+	}
+	
+	///BASIC PROPERTIES///
+	private double width;
+	private double height;
+	
+	
+	///DEFAULTS///
+	private final static double  UPPER_WORLD_BOUND = Double.MAX_VALUE;
+	
+	
+	///SETTERS///
+	
+	public void setWorldWidth(double width){
+		if (width > UPPER_WORLD_BOUND || width < 0)
+			width = UPPER_WORLD_BOUND;
+		this.width = width;
+	}
+	
+	public void setWorldHeight(double height){
+		if (height > UPPER_WORLD_BOUND || height < 0)
+			height = UPPER_WORLD_BOUND;
+		this.height = height;
+	}
+	
 }
