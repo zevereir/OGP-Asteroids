71c83e1ecf49a2465cd9e7a11c78001d40f9676b
Sieben Bocklandt <siebe@192.168.0.205>
2017-03-20 14:00:43 +0100

    added annotations import

--- OGP1617-Asteroids/src/asteroids/model/Bullet.java
+++ OGP1617-Asteroids/src/asteroids/model/Bullet.java
@@ -1,4 +1,5 @@
 package asteroids.model;
+import be.kuleuven.cs.som.annotate.*;
 
 /**
  * a class that describes a bullet that flys away in the blue blue sky
--- OGP1617-Asteroids/src/asteroids/model/World.java
+++ OGP1617-Asteroids/src/asteroids/model/World.java
@@ -1,4 +1,5 @@
 package asteroids.model;
+import be.kuleuven.cs.som.annotate.*;
 /**
  * a class that describes the world 
  * @author sieben
