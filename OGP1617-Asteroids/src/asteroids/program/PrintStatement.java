faf9377e205d09c77e4b3f7b07ede46cabec7f1f
RubenPants <broekxruben@gmail.com>
2017-05-04 21:51:48 +0200

    PrintStatement

--- OGP1617-Asteroids/src/asteroids/program/PrintStatement.java
+++ OGP1617-Asteroids/src/asteroids/program/PrintStatement.java
@@ -2,10 +2,19 @@ package asteroids.program;
 
 class PrintStatement extends MyStatement {
 
+	public PrintStatement(MyExpression expression) {
+		setExpression(expression);
+	}
+	
+	private void setExpression(MyExpression expression) {
+		this.expression = expression;
+	}
+	
+	private MyExpression expression;
+	
 	@Override
 	public void evaluate() {
-		// TODO Auto-generated method stub
-		
+		System.out.println(expression.getExpressionResult());
 	}
 
 	
