cf79a7a54b091d5f68c5993f1f5fd3a493759d0c
RubenPants <broekxruben@gmail.com>
2017-05-06 01:33:56 +0200

    Merged

--- OGP1617-Asteroids/src/asteroids/program/BulletEntity.java
+++ OGP1617-Asteroids/src/asteroids/program/BulletEntity.java
@@ -25,13 +25,16 @@ class BulletEntity extends EntityExpression {
 		
 		// Filter out all the bullets that do not belong to the ship
 		bullets.removeIf(bullet -> !isFiredFromShip((Bullet) bullet));
+		
 		int sizeSet = bullets.size();
+		
 		if (sizeSet != 0) {
-		int randomNumber = new Random().nextInt(sizeSet);
-		int i = 0;
-		for (Object bullet : bullets) {
-			if (i == randomNumber) {
-				return bullet;
+			int randomNumber = new Random().nextInt(sizeSet);
+			int i = 0;
+			
+			for (Object bullet : bullets) {
+				if (i == randomNumber) {
+					return bullet;
 				}
 				i++;
 			}
