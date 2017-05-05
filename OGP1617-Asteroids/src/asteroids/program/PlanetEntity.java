16ea1f8398ef706f50614f891f34807387a51e34
Sieben Bocklandt <siebe@10.46.180.151>
2017-05-05 23:35:39 +0200

    merge

--- OGP1617-Asteroids/src/asteroids/program/PlanetEntity.java
+++ OGP1617-Asteroids/src/asteroids/program/PlanetEntity.java
@@ -3,7 +3,7 @@ package asteroids.program;
 class PlanetEntity extends EntityExpression {
 
 	protected PlanetEntity() throws IllegalArgumentException {
-		setOperand(getClosestEntity(getExpressionShip().getEntityWorld().getWorldSpecificEntities("MinorPlanet")));
+		
 		
 	}
 }
