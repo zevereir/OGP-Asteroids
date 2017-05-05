d6a4ce5c254029e54fde62d521aa733c652ad7e4
Sieben Bocklandt <siebe@10.46.180.151>
2017-05-06 01:12:14 +0200

    Merge

--- OGP1617-Asteroids/src/asteroids/program/AnyEntity.java
+++ OGP1617-Asteroids/src/asteroids/program/AnyEntity.java
@@ -14,20 +14,15 @@ class AnyEntity extends EntityExpression {
 	protected Object getExpressionResult(Program program) {
 		setExpressionProgram(program);
 		
-<<<<<<< HEAD
-		Object operand = null;
-		String random = (fruits[new Random().nextInt(fruits.length)]);
-		do {Set<? extends Object> entities = getExpressionShip().getEntityWorld().getWorldEntities();
-			operand = entities.stream().skip((int)(entities.size() * Math.random())).findFirst();
-		}while (operand == getExpressionShip());
-=======
+
+		
 		Set<? extends Object> entities = getExpressionShip().getEntityWorld().getWorldEntities();
 		entities.remove(getExpressionShip());
 		
 		int sizeSet = entities.size();
 		int randomNumber = new Random().nextInt(sizeSet);
 		int i = 0;
->>>>>>> branch 'master' of https://github.com/zevereir/ZevereirsProject.git
+
 		
 		if (sizeSet != 0) {		
 			for (Object entity : entities) {
