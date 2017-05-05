4443497b50c3286862cc7febeaaee84d21894974
Sieben Bocklandt <siebe@10.46.180.151>
2017-05-06 01:13:47 +0200

    control shift p

--- OGP1617-Asteroids/src/asteroids/program/AnyEntity.java
+++ OGP1617-Asteroids/src/asteroids/program/AnyEntity.java
@@ -8,31 +8,28 @@ import asteroids.model.Entity;
 class AnyEntity extends EntityExpression {
 
 	protected AnyEntity() throws IllegalArgumentException {
-		
+
 	}
-	
+
 	protected Object getExpressionResult(Program program) {
 		setExpressionProgram(program);
-		
 
-		
 		Set<? extends Object> entities = getExpressionShip().getEntityWorld().getWorldEntities();
 		entities.remove(getExpressionShip());
-		
+
 		int sizeSet = entities.size();
 		int randomNumber = new Random().nextInt(sizeSet);
 		int i = 0;
 
-		
-		if (sizeSet != 0) {		
+		if (sizeSet != 0) {
 			for (Object entity : entities) {
 				if (i == randomNumber) {
 					return entity;
 				}
 				i++;
 			}
-		} 
-		
+		}
+
 		return null;
 	}
 }
