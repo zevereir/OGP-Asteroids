531cc568d1b7dc9845dadaa14e08f2b6b73a8120
RubenPants <broekxruben@gmail.com>
2017-05-05 18:20:21 +0200

    Use it.

---
+++ OGP1617-Asteroids/tests/asteroids/tests/Test_test_test_test_stayingAlive_stayingAlive.java
@@ -0,0 +1,64 @@
+package asteroids.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import asteroids.model.*;
+import asteroids.facade.Facade;
+import asteroids.part3.facade.IFacade;
+import asteroids.part3.programs.IProgramFactory;
+import asteroids.part3.programs.internal.ProgramParser;
+import asteroids.program.Program;
+import asteroids.program.ProgramFactory;
+import asteroids.util.ModelException;
+
+/**
+ * DO NOT COMMIT THIS TEST-FILE
+ * 
+ * @version 5th of may
+ * @authors Sieben Bocklandt and Ruben Broekx
+ *
+ */
+public class Test_test_test_test_stayingAlive_stayingAlive {
+
+	/// CONSTANTS ///
+	
+	private final static double SPEED_OF_LIGHT = 300000;
+	private static final double EPSILON = 0.0001;
+
+	IFacade facade;
+
+	@Before
+	public void setUp() {
+		facade = new Facade();
+	}
+
+	public Ship[] createShips() throws ModelException {
+		Ship ship1 = facade.createShip(10000, 10000, 0, 0, 10, 0,5E16);
+		Ship ship2 = facade.createShip(10000, 10100, 0, -10, 10, 3 * Math.PI / 2,5E16);
+		Ship ship3 = facade.createShip(10100, 10000, -10, 0, 10, Math.PI,0);
+		Ship ship4 = facade.createShip(10000, 9900, 0, 10, 10, Math.PI / 2,0);
+		Ship ship5 = facade.createShip(9900, 10000, 10, 0, 10, 0,0);
+		Ship ship6 = facade.createShip(1000, 1000, 10, 0, 100,0,0);
+		Ship ship7 = facade.createShip(10000, 10000, SPEED_OF_LIGHT, SPEED_OF_LIGHT, 100, Math.PI / 4,0);
+		Ship ship8 = facade.createShip(10001,10001,0,0,100,0,0);
+		Ship ship9 = facade.createShip(0,0,0,0,10,0,0);
+		Ship ship10 = facade.createShip(29000.0,10000.0,200,0,200,0,0);
+		Ship ship11 = facade.createShip(10000,29000,0,200,200,0,0);
+		Ship ship12 = facade.createShip(10000,10000,1000,1500,1000,0,0);
+		Ship ship13 = facade.createShip(10000,12050,0,0,1000,3*Math.PI/2,0);
+		
+		Ship[] Total = { ship1, ship2, ship3, ship4, ship5, ship6, ship7,ship8,ship9,ship10,ship11,ship12,ship13};
+		return Total;
+	}
+	
+	
+	
+
+}
