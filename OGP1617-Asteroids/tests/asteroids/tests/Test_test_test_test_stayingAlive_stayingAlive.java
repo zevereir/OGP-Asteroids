aa58293d55adb318b8e6701fb6d34516170e42ab
Sieben Bocklandt <siebe@10.1.72.21>
2017-05-06 15:05:44 +0200

    new test

--- OGP1617-Asteroids/tests/asteroids/tests/Test_test_test_test_stayingAlive_stayingAlive.java
+++ OGP1617-Asteroids/tests/asteroids/tests/Test_test_test_test_stayingAlive_stayingAlive.java
@@ -73,14 +73,15 @@ public class Test_test_test_test_stayingAlive_stayingAlive {
 
 
 	@Test
-	  public void testReadParameter_LegalCase() throws ModelException {
-	    max_score += 8;
-	    String code = "def f {" + "  return $1; " + "}" + "print f(22.0); ";
-	    Program program = ProgramParser.parseProgramFromString(code, programFactory);
-	    facade.loadProgramOnShip(ship1, program);
-	    List<Object> results = facade.executeProgram(ship1, 1.0);
-	    Object[] expecteds = { 22.0 };
-	    assertArrayEquals(expecteds, results.toArray());
-	    score += 8;
+	  public void testFunctionCall_NotEnoughActualArguments() throws ModelException {
+	    try {
+	      max_score += 6;
+	      String code = "def f { " + "  return $1 + $2; " + "}" + "print f(3.0); ";
+	      Program program = ProgramParser.parseProgramFromString(code, programFactory);
+	      facade.loadProgramOnShip(ship1, program);
+	      facade.executeProgram(ship1, 0.3);
+	    } catch (ModelException exc) {
+	      score += 6;
+	    }
 	  }
 }
