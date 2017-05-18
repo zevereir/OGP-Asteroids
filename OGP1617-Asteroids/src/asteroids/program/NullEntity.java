4e1efcc7f69303ebd8b73dcfc52f8c3bce806a78
RubenPants <broekxruben@gmail.com>
2017-05-18 20:10:57 +0200

    Layout

--- OGP1617-Asteroids/src/asteroids/program/NullEntity.java
+++ OGP1617-Asteroids/src/asteroids/program/NullEntity.java
@@ -5,14 +5,14 @@ import java.util.List;
 class NullEntity extends EntityExpression {
 
 	/// CONSTRUCTOR ///
-	
+
 	protected NullEntity() throws IllegalArgumentException {
 		//
 	}
 
 	
 	/// GETTERS ///
-	
+
 	protected Object getExpressionResult(Program program, List<MyExpression> actualArgs, MyFunction function) {
 		return null;
 	}
