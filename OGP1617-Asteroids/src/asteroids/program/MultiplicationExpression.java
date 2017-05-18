a4d3933f66a26271cbf8f7d4497ec26c1162a11d
RubenPants <broekxruben@gmail.com>
2017-05-18 20:14:48 +0200

    Layout

--- OGP1617-Asteroids/src/asteroids/program/MultiplicationExpression.java
+++ OGP1617-Asteroids/src/asteroids/program/MultiplicationExpression.java
@@ -3,7 +3,7 @@ package asteroids.program;
 import java.util.List;
 
 class MultiplicationExpression extends BinaryExpression<ArithmeticExpression> implements ArithmeticExpression {
-	
+
 	/// CONSTRUCTOR ///
 
 	protected MultiplicationExpression(ArithmeticExpression leftExpression, ArithmeticExpression rightExpression)
@@ -17,13 +17,13 @@ class MultiplicationExpression extends BinaryExpression<ArithmeticExpression> im
 	@Override
 	protected Object getExpressionResult(Program program, List<MyExpression> actualArgs, MyFunction function) {
 		setExpressionProgram(program);
-		
+
 		BinaryArithmeticExpression solved = new BinaryArithmeticExpression();
-		
-		Double leftOperand = (double)solved.solveLeftOperand(program, actualArgs, function);
-		Double rightOperand = (double)solved.solveRightOperand(program, actualArgs, function);
+
+		Double leftOperand = (double) solved.solveLeftOperand(program, actualArgs, function);
+		Double rightOperand = (double) solved.solveRightOperand(program, actualArgs, function);
 
 		return leftOperand * rightOperand;
 	}
-	
+
 }
