8cb110ffc5e5b8997f26fe4ebc34bc18d71e6344
RubenPants <broekxruben@gmail.com>
2017-05-18 20:42:58 +0200

    Layout

--- OGP1617-Asteroids/src/asteroids/program/BreakStatement.java
+++ OGP1617-Asteroids/src/asteroids/program/BreakStatement.java
@@ -3,14 +3,14 @@ package asteroids.program;
 import java.util.List;
 
 class BreakStatement extends MyStatement {
-	
+
 	/// CONSTRUCTOR ///
-	
+
 	public BreakStatement() {
 		//
-	}
+	}	
 	
-	
+
 	/// EVALUATION ///
 
 	protected void evaluate(Program program, List<MyExpression> actualArgs) {
@@ -20,4 +20,5 @@ class BreakStatement extends MyStatement {
 	protected Object evaluateInFunction(Program program, List<MyExpression> actualArgs, MyFunction function) {
 		throw new IllegalAccessError();
 	}
+
 }
